2516667b425d83d365d83fc9d7b7dae819dcd740
awaji-mitop GILBERT <successgilli@gmail.com>
2020-04-12 11:07:01 -0700

    Updates

---
+++ precache-manifest.54228f181a7994988f2b21b0abac6800.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f74e80dcaae76a2e1ce96168a327eba0",
+    "url": "/seniorDevUdemy/index.html"
+  },
+  {
+    "revision": "660907bf906e51ebd5fb",
+    "url": "/seniorDevUdemy/static/css/3.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "12ad9a9e40c764522e94",
+    "url": "/seniorDevUdemy/static/css/main.83909ae8.chunk.css"
+  },
+  {
+    "revision": "04875003cd503100a77f",
+    "url": "/seniorDevUdemy/static/js/2.1935e29a.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/seniorDevUdemy/static/js/2.1935e29a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "660907bf906e51ebd5fb",
+    "url": "/seniorDevUdemy/static/js/3.8f71ff7f.chunk.js"
+  },
+  {
+    "revision": "1690382abbc8bf30c651",
+    "url": "/seniorDevUdemy/static/js/4.c3dc44c2.chunk.js"
+  },
+  {
+    "revision": "12ad9a9e40c764522e94",
+    "url": "/seniorDevUdemy/static/js/main.883f04ee.chunk.js"
+  },
+  {
+    "revision": "040fd91c7dbc13ca3ac2",
+    "url": "/seniorDevUdemy/static/js/runtime-main.d26a4f93.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/seniorDevUdemy/static/media/SEGA.82e09f78.TTF"
+  }
+]);
